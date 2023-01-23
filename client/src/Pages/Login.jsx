83c83f2c4a4ea2ee31b2b90aafde2e143b1dac46
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2023-01-23 12:42:37 -0600

    .

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -6,7 +6,8 @@ import {useNavigate} from "react-router-dom";
 function Login(props) {
     const usuario = useSelector((state) => state.usuario)
     const navigate = useNavigate()
-    const [logged, ] = useState(usuario.value ? usuario.value.token : null);
+    const [logged, ] = useState(usuario.value && usuario.value.token ? usuario.value.token : null);
+    console.log(logged)
     useEffect(() => {
            if(logged !== undefined) {
                navigate('/home')
